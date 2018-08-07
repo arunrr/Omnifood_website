36ab3be09c1bc2de974abc5fdd23d2f4a6887705
nikorockz <sparkingkings@gmail.com>
2018-08-07 18:12:33 +0530

    Added some scroll animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -46,4 +46,23 @@ $('a[href*="#"]')
       }
     }
   });
+    
+    
+//    Scroll animations
+    
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{ offset: "50%"});
+    
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{ offset: "50%"});
+    
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{ offset: "50%"});
+    
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    },{ offset: "50%"});
 });
